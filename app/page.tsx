633f1fb3174431463f8cdda73b3abccf74b58649
Adam Fratino <fratino.a@gmail.com>
2024-07-21 01:48:11 -0400

    fix: broken import

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,30 @@
 import { Metadata } from "next"
-import { Button } from "@/components/Button/Button"
 
 export const metadata: Metadata = {
   title: "Tekken.lol",
-  twitter: {
-    card: "summary_large_image",
-  },
-  openGraph: {
-    url: "https://tekken.lol/",
-    images: [
-      {
-        width: 1200,
-        height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
-      },
-    ],
-  },
+  // twitter: {
+  //   card: "summary_large_image",
+  // },
+  // openGraph: {
+  //   url: "https://tekken.lol/",
+  //   images: [
+  //     {
+  //       width: 1200,
+  //       height: 630,
+  //       url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
+  //     },
+  //   ],
+  // },
 }
 
 export default function Web() {
   return (
     <>
-      <header className="bg-white dark:bg-gray-900">
+      <header>
         <nav>nav</nav>
       </header>
-      <main className="bg-white dark:bg-gray-900">main</main>
+      <main>main</main>
+      <aside>aside</aside>
       <footer>footer</footer>
     </>
   )
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react"
 import { cn } from "@/lib/utils"
 import { styles } from "./button.styles"
 import * as variants from "./button.variants"
