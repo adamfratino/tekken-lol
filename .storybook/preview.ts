8deb3fdfea08c8b85c3a485a4afc9faa739aa568
Adam Fratino <fratino.a@gmail.com>
2024-07-21 01:16:33 -0400

    refactor(button): clean up button poc

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from "@storybook/react"
-
-import "../styles/tailwind.css"
+import "@radix-ui/themes/styles.css"
+import "../styles/globals.css"
 
 const preview: Preview = {
   tags: ["autodocs"],
