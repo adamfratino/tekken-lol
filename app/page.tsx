826d319d6ce9b306e44f477108928195db2e1063
Adam Fratino <fratino.a@gmail.com>
2024-09-06 00:24:50 -0400

    fix: disable user scale

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export const metadata = {
     },
   ],
   viewport:
-    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover user-scalable=0",
   icons: [
     { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
     { rel: "icon", url: "icons/icon-128x128.png" },
