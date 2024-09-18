386009ac80d13c01b70067c8a859bf6824ffe108
Adam Fratino <fratino.a@gmail.com>
2024-09-17 23:26:51 -0400

    fix: mobile header shift

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -15,7 +15,7 @@ type HeaderProps = {
 }
 
 export const Header = ({ character, moves }: HeaderProps) => (
-  <header className="flex h-14 w-full items-center gap-3 border-b bg-muted px-2 lg:h-[60px] lg:px-4">
+  <header className="sticky top-0 z-10 flex h-14 w-full items-center gap-3 border-b bg-muted px-2 lg:h-[60px] lg:px-4">
     <MobileHamburgerButton character={character} />
     <DesktopCharacterSelectButton character={character} />
 
