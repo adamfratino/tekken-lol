8bfc159afb502e4cfb21639c3f16975d5e670e37
Adam Fratino <fratino.a@gmail.com>
2024-09-19 01:03:32 -0400

    fix: mobile sticky headlines

--- app/components/MovesTable/subcomponents/MovesTableHeadline.tsx
+++ app/components/MovesTable/subcomponents/MovesTableHeadline.tsx
@@ -4,7 +4,7 @@ export const MovesTableHeadline = ({ text }: { text: string }) => (
   <Title
     as="h2"
     size="md"
-    className="sticky top-0 z-10 bg-foreground px-4 py-2 text-sm text-background md:px-6 md:text-base"
+    className="sticky top-[56px] z-10 bg-foreground px-4 py-2 text-sm text-background md:px-6 md:text-base"
   >
     {text}
   </Title>
